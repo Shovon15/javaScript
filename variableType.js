08b69d6abed2165e5881d3e2cacf35e6729bdcc8
Shovon15 <shovonrnbd@gmail.com>
2022-08-05 22:26:09 +0600

    variables

---
+++ variableType.js
@@ -0,0 +1,41 @@
+/*
+01. types of variables--------- 
+
+
+
+*/
+
+// 01.types of variables
+
+var num = 20;
+var text = "shovon";
+var con = true;
+// console.log(typeof(num));
+// console.log(typeof(text));
+// console.log(typeof(con));
+
+
+let pInt = 3.14;
+// console.log(typeof(pInt));
+// console.log(parseInt(pInt));
+
+let pFloat = 320;
+// console.log(typeof(pFloat));
+// console.log(parseFloat(pFloat));
+
+// console.log(parseFloat(3.14));
+
+let number = 45;
+// console.log(number.toExponential(2));
+// console.log(number.toFixed(3));
+// console.log(typeof(number.toFixed(3)));
+// console.log(number.toPrecision(4));
+
+
+let man = "sumit";
+// console.log(typeof(Number(true)));
+// console.log(Number(true));
+
+
+
+console.log(parseFloat("10.33"));
